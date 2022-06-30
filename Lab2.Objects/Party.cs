4b7d6ac25a5a6b21b57ec03a332201dacf2c802d
povolyaev <106241125+povolyaev@users.noreply.github.com>
2022-06-30 23:10:57 +0300

    Add files via upload

---
+++ Lab2.Objects/Party.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Objects
+{
+    public class Party
+    {
+        public string name;
+        public int amount_of_people;
+        public double percentage_of_votes;
+
+        public Party()
+        {
+            name = "";
+            amount_of_people = 0;
+            percentage_of_votes = 0;
+        }
+
+        public void set_name(string n)
+        {
+            name = n;
+        }
+
+        public void set_amount(int amount)
+        {
+            if (amount > 0)
+                amount_of_people = amount;
+            else
+                amount_of_people = 0;
+        }
+
+        public void set_percentage(double percent)
+        {
+            if (percent > 0)
+                percentage_of_votes = percent;
+            else
+                percentage_of_votes = 0;
+        }
+
+        public string get_name()
+        { return name; }
+
+        public int get_amount()
+        { return amount_of_people; }
+
+        public double get_percentage()
+        { return percentage_of_votes; }
+
+        public double q;
+        public void count_q()
+        {
+            q = 0.3 * amount_of_people + 0.7 * percentage_of_votes;
+        }
+
+        public string print()
+        {
+            count_q();
+            return "Партия " + name + "  численностью " + amount_of_people + " человек и процентом голосов " + percentage_of_votes + "%.\nПараметр Q = " + q;
+        }
+    }
+}
---
+++ Lab2.Objects/Party_last_year.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Objects
+{
+    public class Party_last_year : Party
+    {
+        public int last_year_amount;
+
+        public void set_last_year_amount(int amount)
+        {
+            if (amount > 0)
+                last_year_amount = amount;
+            else
+                last_year_amount = 0;
+        }
+
+        public int get_last_year_amount()
+        { return last_year_amount; }
+
+        public double qp;
+        void count_qp()
+        {
+            count_q();
+            if (amount_of_people > last_year_amount)
+                qp = 1.2 * q;
+            else
+                qp = 0.8 * q;
+        }
+
+        new public string print()
+        {
+            count_q();
+            count_qp();
+            return "Партия " + name + "  численностью " + amount_of_people + " человек и процентом голосов " + percentage_of_votes + "%.\nПараметр Q = " + q + " Параметр p = " + last_year_amount + " Параметр qp = " + qp;
+        }
+    }
+}
